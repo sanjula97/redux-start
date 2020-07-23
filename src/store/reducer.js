b7a4050aefe683f7dba6cf75ffe8927e7c7db979
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-23 23:18:48 +0530

    Updated the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,30 +3,26 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
-    return {
-      counter: state.counter + 1,
-    };
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      };
+    case "ADD_5":
+      return {
+        counter: state.counter + action.value,
+      };
+    case "SUB_5":
+      return {
+        counter: state.counter - action.value,
+      };
+    default:
+      return state;
   }
-
-  if (action.type === "DECREMENT") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-
-  if (action.type === "ADD_5") {
-    return {
-      counter: state.counter + action.value,
-    };
-  }
-
-  if (action.type === "SUB_5") {
-    return {
-      counter: state.counter - action.value,
-    };
-  }
-  return state;
 };
 
 export default reducer;
