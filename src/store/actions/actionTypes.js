09cd565a62f217c56800e51e617fbeef5797b2d4
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-08-01 06:44:10 +0530

    Destructured results file

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -4,25 +4,3 @@ export const ADD_5 = "ADD_5";
 export const SUB_5 = "SUB_5";
 export const STORE_RESULT = "STORE_RESULT";
 export const DELETE_RESULT = "DELETE_RESULT";
-
-export const saveResult = (result) => {
-  return {
-    type: STORE_RESULT,
-    result: result,
-  };
-};
-
-export const storeResult = (result) => {
-  return (dispatch) => {
-    setTimeout(() => {
-      dispatch(saveResult(result));
-    }, 2000);
-  };
-};
-
-export const deleteResult = (id) => {
-  return {
-    type: DELETE_RESULT,
-    resultelId: id,
-  };
-};
